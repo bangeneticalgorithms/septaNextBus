a28e16320842b560654b2a1d2b345f4796cad1ac
iud23drexel@gmail.com <iud23@drexel.edu>
2015-12-19 09:08:37 -0800

    Added trainer_fann.h

---
+++ src/trainer_fann.h
@@ -0,0 +1,19 @@
+#ifndef TRAINER_FANN_H
+#define TRAINER_FANN_H
+
+#include "trainer_base.h"
+
+class TrainerFANN : public TrainerBase
+{
+public:
+	TrainerFANN() {}
+	~TrainerFANN() {}
+	
+	double trainData(vector< vector<double> > &train_data, vector<double> &theta_results, vector<double> &mu_results, vector<double> &sigma_results);
+	
+	void featureNormalize(vector< vector<double> > &train_data, vector<double> &mu_results, vector<double> &sigma_results);
+	
+	static void writeFANNInputData(vector< vector<double> > &train_data, string filename);
+};
+
+#endif
