58b9e51ec8cb4c6c23f94020b0dae7b25a8888fd
iud23drexel@gmail.com <iud23@drexel.edu>
2015-12-20 09:08:39 -0800

    Added gtfs

---
+++ src/gtfs.h
@@ -0,0 +1,17 @@
+#ifndef GTFS_H
+#define GTFS_H
+
+enum GTFS_DB_TYPE
+{
+	GTFS_TRIPS_DB,
+	GTFS_STOP_TIMES_DB,
+	GTFS_STOPS_DB,
+	GTFS_ROUTES_DB,
+	GTFS_CALENDAR_DB,
+	GTFS_DB_TYPE_MAX
+};
+
+void load_gtfs(bool force_reload = false);
+void load_gtfs_file(int db_type, bool force_reload = false);
+
+#endif
